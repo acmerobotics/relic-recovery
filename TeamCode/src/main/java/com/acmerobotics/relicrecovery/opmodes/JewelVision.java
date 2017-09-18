84c07218050802e274ea7083a17e7aa5a83ce7b1
Ryan Brott <rcbrott@gmail.com>
2017-09-17 21:10:05 -0700

    RR-10 clean up the code and add some clarifying comments

--- TeamCode/src/main/java/com/acmerobotics/relicrecovery/opmodes/JewelVision.java
+++ TeamCode/src/main/java/com/acmerobotics/relicrecovery/opmodes/JewelVision.java
@@ -32,7 +32,6 @@ import android.app.Activity;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
-import android.os.Environment;
 import android.util.Log;
 import android.view.View;
 import android.widget.FrameLayout;
@@ -48,7 +47,6 @@ import com.vuforia.Vuforia;
 
 import org.firstinspires.ftc.robotcore.external.ClassFactory;
 import org.firstinspires.ftc.robotcore.external.navigation.VuforiaLocalizer;
-import org.firstinspires.ftc.robotcore.internal.opengl.AutoConfigGLSurfaceView;
 import org.firstinspires.ftc.robotcore.internal.system.AppUtil;
 import org.firstinspires.ftc.teamcode.R;
 import org.opencv.android.BaseLoaderCallback;
@@ -61,7 +59,6 @@ import org.opencv.core.Rect;
 import org.opencv.core.Scalar;
 import org.opencv.imgproc.Imgproc;
 
-import java.io.File;
 import java.nio.ByteBuffer;
 import java.util.Arrays;
 import java.util.concurrent.BlockingQueue;
@@ -77,10 +74,8 @@ public class JewelVision extends LinearOpMode {
     private Mat raw, rgb;
     private byte[] imgData;
     private CountDownLatch openCvInitialized = new CountDownLatch(1);
-    private File imageSaveLocation;
     VuforiaLocalizer vuforia;
     BlockingQueue<VuforiaLocalizer.CloseableFrame> frameQueue;
-    private int surfaceWidth, surfaceHeight;
     private int imageWidth, imageHeight;
     private Rect jewelPlatformRect, leftJewelRect, rightJewelRect;
     private double leftRed, leftBlue, rightRed, rightBlue;
@@ -93,43 +88,39 @@ public class JewelVision extends LinearOpMode {
     @Override public void runOpMode() throws InterruptedException {
         telemetry = new MultipleTelemetry(Arrays.asList(telemetry, RobotDashboard.getInstance().getTelemetry()));
 
+        final Paint paint = new Paint();
+        paint.setStyle(Paint.Style.STROKE);
+        paint.setStrokeWidth(5.0f);
+
         final View overlayView = new View(hardwareMap.appContext) {
             @Override
             protected void onDraw(Canvas canvas) {
                 super.onDraw(canvas);
 
-                Paint paint = new Paint();
-                paint.setStyle(Paint.Style.STROKE);
-                paint.setStrokeWidth(5.0f);
-
+                // compute the portion of the canvas that overlaps the camera preview
                 int canvasWidth = canvas.getWidth(), canvasHeight = canvas.getHeight();
-                float canvasWidthHeightRatio = (float) canvasWidth / canvasHeight;
+                float canvasAspectRatio = (float) canvasWidth / canvasHeight;
 
-                float surfaceViewWidthHeightRatio = (float) surfaceWidth / surfaceHeight;
+                float imageAspectRatio = (float) imageHeight / imageWidth;
 
-                int width, height;
-                if (canvasWidthHeightRatio > surfaceViewWidthHeightRatio) {
+                int overlayWidth, overlayHeight;
+                if (canvasAspectRatio > imageAspectRatio) {
                     // width is bigger
-                    width = canvasHeight;
-                    height = (int) (canvasHeight * surfaceViewWidthHeightRatio);
+                    overlayWidth = canvasHeight;
+                    overlayHeight = (int) (canvasHeight * imageAspectRatio);
                 } else {
                     // height is bigger
-                    height = canvasWidth;
-                    width = (int) (canvasWidth / surfaceViewWidthHeightRatio);
+                    overlayHeight = canvasWidth;
+                    overlayWidth = (int) (canvasWidth / imageAspectRatio);
                 }
 
-//                Log.i(TAG, String.format("canvas: %d x %d", canvasWidth, canvasHeight));
-//                Log.i(TAG, String.format("view: %d x %d", getWidth(), getHeight()));
-//                Log.i(TAG, String.format("image: %d x %d", imageWidth, imageHeight));
-//                Log.i(TAG, String.format("surface: %d x %d", surfaceWidth, surfaceHeight));
-//                Log.i(TAG, String.format("image_on_canvas: %d x %d", width, height));
-//                Log.i(TAG, String.format("scale: %f, %f", (float) height / imageHeight, (float) width / imageWidth));
-
+                // transform the canvas so that image coordinates can be used
                 canvas.translate(canvasWidth / 2.0f, canvasHeight / 2.0f);
                 canvas.rotate(90.0f);
-                canvas.scale((float) height / imageHeight, (float) width / imageWidth);
+                canvas.scale((float) overlayHeight / imageHeight, (float) overlayWidth / imageWidth);
                 canvas.translate(-imageWidth / 2.0f, -imageHeight / 2.0f);
 
+                // draw the jewel rects
                 if (jewelPlatformRect != null) {
                     paint.setColor(Color.GREEN);
                     drawOpenCvRect(canvas, jewelPlatformRect, paint);
@@ -154,9 +145,6 @@ public class JewelVision extends LinearOpMode {
                 }
             }
         };
-
-        imageSaveLocation = new File(Environment.getExternalStorageDirectory(), "ACME");
-        imageSaveLocation.mkdirs();
 
         final BaseLoaderCallback loaderCallback = new BaseLoaderCallback(hardwareMap.appContext) {
             @Override
@@ -194,9 +182,6 @@ public class JewelVision extends LinearOpMode {
             @Override
             public void run() {
                 LinearLayout cameraMonitorView = (LinearLayout) activity.findViewById(R.id.cameraMonitorViewId);
-                AutoConfigGLSurfaceView surfaceView = (AutoConfigGLSurfaceView) cameraMonitorView.getChildAt(0);
-                surfaceWidth = surfaceView.getWidth();
-                surfaceHeight = surfaceView.getHeight();
                 parentLayout = (FrameLayout) cameraMonitorView.getParent();
                 parentLayout.addView(overlayView);
             }
@@ -210,7 +195,7 @@ public class JewelVision extends LinearOpMode {
         waitForStart();
 
         while (opModeIsActive()) {
-            // Grab frames
+            // grab frames and process them
             if (!frameQueue.isEmpty()) {
                 VuforiaLocalizer.CloseableFrame frame = frameQueue.take();
                 for (int i = 0; i < frame.getNumImages(); i++) {
@@ -238,8 +223,6 @@ public class JewelVision extends LinearOpMode {
                         }
                         raw.put(0, 0, imgData);
                         Imgproc.cvtColor(raw, rgb, Imgproc.COLOR_RGB2BGR);
-
-//                        Imgcodecs.imwrite(new File(imageSaveLocation, System.currentTimeMillis() + ".jpg").getPath(), rgb);
 
                         Mat leftJewelCropped = rgb.submat(leftJewelRect);
                         Mat rightJewelCropped = rgb.submat(rightJewelRect);
@@ -271,6 +254,7 @@ public class JewelVision extends LinearOpMode {
                             rightRed /= rightTotal;
                         }
 
+                        // make sure to release mats afterward; memory leaks are bad and annoying
                         rgb.release();
                         raw.release();
                     }
