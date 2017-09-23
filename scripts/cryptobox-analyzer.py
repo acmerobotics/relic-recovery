c3d727c15dc80dc031e423e5a8268d75f18c4d50
Ryan Brott <rcbrott@gmail.com>
2017-09-23 13:31:30 -0700

    RR-20 update cryptobox python pipeline and add more test images

--- scripts/cryptobox-analyzer.py
+++ scripts/cryptobox-analyzer.py
@@ -3,51 +3,92 @@ import numpy as np
 import os
 
 RED_LOWER_HSV, RED_UPPER_HSV = (0, 80, 0), (7, 255, 255)
-BLUE_LOWER_HSV, BLUE_UPPER_HSV = (110, 80, 0), (120, 255, 255)
+RED2_LOWER_HSV, RED2_UPPER_HSV = (170, 80, 0), (180, 255, 255)
+BLUE_LOWER_HSV, BLUE_UPPER_HSV = (110, 80, 0), (130, 255, 255)
 
-KERNEL_SIZE = 11
+OPEN_KERNEL_SIZE, CLOSE_KERNEL_SIZE = 7, 15
 
-# IMAGE_FILENAME = 'cryptobox/blue001.jpg'
 IMAGE_DIR = 'cryptobox/'
-WIDTH, HEIGHT = 576, 480 # 4:3 aspect ratio
+MIN_DIMENSION = 480
+
+
+def format_num(num):
+    if num >= 10**6:
+        return '{:.1f}M'.format(num / 10**6)
+    elif num >= 10**3:
+        return '{:.1f}K'.format(num / 10**3)
+    else:
+        return str(num)
+
 
 os.makedirs('cryptobox2/', exist_ok=True)
-for filename in os.listdir(IMAGE_DIR):
-    if filename.startswith('red'):
-        lower_hsv, upper_hsv = RED_LOWER_HSV, RED_UPPER_HSV
-    else:
-        lower_hsv, upper_hsv = BLUE_LOWER_HSV, BLUE_UPPER_HSV
-
+for i, filename in enumerate(os.listdir(IMAGE_DIR)):
     image = cv2.imread(IMAGE_DIR + filename)
-    image = cv2.resize(image, (WIDTH, HEIGHT), interpolation=cv2.INTER_CUBIC)
+    height, width = image.shape[:2]
+    if height < width:
+        new_height = MIN_DIMENSION
+        new_width = int(width / height * new_height)
+    else:
+        new_width = MIN_DIMENSION
+        new_height = int(height / width * new_width)
+    image = cv2.resize(image, (new_width, new_height), interpolation=cv2.INTER_CUBIC)
+
     hsv = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
-    mask = cv2.inRange(hsv, lower_hsv, upper_hsv)
+    red = cv2.inRange(hsv, RED_LOWER_HSV, RED_UPPER_HSV)
+    red2 = cv2.inRange(hsv, RED2_LOWER_HSV, RED2_UPPER_HSV)
+    blue = cv2.inRange(hsv, BLUE_LOWER_HSV, BLUE_UPPER_HSV)
 
-    kernel = np.ones((KERNEL_SIZE, KERNEL_SIZE), np.uint8)
-    morphed_mask = cv2.morphologyEx(mask, cv2.MORPH_OPEN, kernel)
-    morphed_mask = cv2.morphologyEx(morphed_mask, cv2.MORPH_CLOSE, kernel, iterations=3)
+    cv2.bitwise_or(red, red2, red)
 
-    im2, contours, hierarchy = cv2.findContours(morphed_mask, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+    open_kernel = np.ones((OPEN_KERNEL_SIZE, OPEN_KERNEL_SIZE), np.uint8)
+    close_kernel = np.ones((CLOSE_KERNEL_SIZE, CLOSE_KERNEL_SIZE), np.uint8)
+    red_morph = cv2.morphologyEx(red, cv2.MORPH_OPEN, open_kernel)
+    red_morph = cv2.morphologyEx(red_morph, cv2.MORPH_CLOSE, close_kernel)
+    blue_morph = cv2.morphologyEx(blue, cv2.MORPH_OPEN, open_kernel)
+    blue_morph = cv2.morphologyEx(blue_morph, cv2.MORPH_CLOSE, close_kernel)
 
-    # output = np.zeros((HEIGHT, WIDTH, 3), np.uint8)
-    # output[:,:,0] = morphed_mask
-    # output[:,:,1] = morphed_mask
-    # output[:,:,2] = morphed_mask
+    red_contours = cv2.findContours(red_morph, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)[1]
+    blue_contours = cv2.findContours(blue_morph, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)[1]
 
-    for contour in contours:
+    output = image.copy()
+
+    red_areas = []
+    blue_areas = []
+
+    for contour in red_contours:
+        area = int(cv2.contourArea(contour))
         rect = cv2.boundingRect(contour)
-        if (rect[2] / rect[3]) > 0.35:
-            cv2.drawContours(image, [contour], 0, (255, 0, 255), 3)
+        if (rect[2] / rect[3]) > 0.5 or area < 750:
             continue
-        cv2.drawContours(image, [contour], 0, (0, 255, 255), 3)
-        [vx, vy, x, y] = cv2.fitLine(contour, cv2.DIST_L2, 0, 0.01, 0.01)
-        lefty = int((-x * vy / vx) + y)
-        righty = int(((HEIGHT - x) * vy / vx) + y)
-        cv2.line(image, (HEIGHT - 1, righty), (0, lefty), (0, 255, 0), 2)
+        red_areas.append(area)
+        cv2.drawContours(output, [contour], 0, (0, 0, 255), 3)
+        vx, vy, x, y = cv2.fitLine(contour, cv2.DIST_L2, 0, 0.01, 0.01)
+        cv2.line(output, (x, 0), (x, new_height), (0, 255, 255), 2)
 
-    # for contour in contours:
-    #     epsilon = 0.01 * cv2.arcLength(contour, True)
-    #     approx = cv2.approxPolyDP(contour, epsilon, True)
-    #     cv2.drawContours(image, [approx], 0, (255, 255, 0), 3)
+    for contour in blue_contours:
+        area = int(cv2.contourArea(contour))
+        rect = cv2.boundingRect(contour)
+        if (rect[2] / rect[3]) > 0.5 or area < 750:
+            continue
+        blue_areas.append(area)
+        cv2.drawContours(output, [contour], 0, (255, 0, 0), 3)
+        vx, vy, x, y = cv2.fitLine(contour, cv2.DIST_L2, 0, 0.01, 0.01)
+        cv2.line(output, (x, 0), (x, new_height), (255, 255, 0), 2)
 
-    cv2.imwrite('cryptobox2/' + filename, image)
+    red_areas.sort(reverse=True)
+    blue_areas.sort(reverse=True)
+
+    for j, area in enumerate(red_areas):
+        cv2.putText(output, format_num(area), (0, 30 * (j + 1)), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 255), 8)
+        cv2.putText(output, format_num(area), (0, 30 * (j + 1)), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 2)
+    for j, area in enumerate(blue_areas):
+        cv2.putText(output, format_num(area), (0, 30 * (j + 1 + len(red_areas))), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 255), 8)
+        cv2.putText(output, format_num(area), (0, 30 * (j + 1 + len(red_areas))), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 0, 0), 2)
+
+    base_name = filename.split('.')[0]
+    cv2.imwrite('cryptobox2/{}_0.jpg'.format(base_name), image)
+    cv2.imwrite('cryptobox2/{}_1_red_thresh.jpg'.format(base_name), red)
+    cv2.imwrite('cryptobox2/{}_3_blue_thresh.jpg'.format(base_name), blue)
+    cv2.imwrite('cryptobox2/{}_2_red_morph.jpg'.format(base_name), red_morph)
+    cv2.imwrite('cryptobox2/{}_4_blue_morph.jpg'.format(base_name), blue_morph)
+    cv2.imwrite('cryptobox2/{}_5_output.jpg'.format(base_name), output)
--- scripts/hsv-analyzer.py
+++ scripts/hsv-analyzer.py
@@ -1,23 +1,53 @@
 import cv2
+import numpy as np
 from matplotlib import pyplot as plt
 
 IMAGE_FILENAME = 'cryptobox/red004.jpg'
-WIDTH, HEIGHT = 576, 480 # 4:3 aspect ratio
+MIN_DIMENSION = 480
+LOWER_HSV, UPPER_HSV = (170, 80, 0), (7, 255, 255)
 
 image = cv2.imread(IMAGE_FILENAME)
-image = cv2.resize(image, (WIDTH, HEIGHT), interpolation=cv2.INTER_CUBIC)
+height, width = image.shape[:2]
+if height < width:
+    new_height = MIN_DIMENSION
+    new_width = int(width / height * new_height)
+else:
+    new_width = MIN_DIMENSION
+    new_height = int(height / width * new_width)
+image = cv2.resize(image, (new_width, new_height), interpolation=cv2.INTER_CUBIC)
+
 hsv = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
+if LOWER_HSV[0] > UPPER_HSV[0]:
+    mask = cv2.inRange(hsv, LOWER_HSV, (180, UPPER_HSV[1], UPPER_HSV[2]))
+    mask2 = cv2.inRange(hsv, (0, LOWER_HSV[1], LOWER_HSV[2]), UPPER_HSV)
+    cv2.bitwise_or(mask, mask2, mask)
+else:
+    mask = cv2.inRange(hsv, LOWER_HSV, UPPER_HSV)
+
 hue_hist = cv2.calcHist([hsv], [0], None, [180], [0, 180])
 
-plt.subplot(2, 2, 1)
+plt.gcf().canvas.set_window_title(IMAGE_FILENAME)
+
+plt.subplot(2, 3, 1)
 plt.plot(hue_hist)
 plt.xlim([0, 180])
 plt.title('Hue Histogram')
 
 HSV_TITLES = ['Hue', 'Saturation', 'Value']
 for i in range(3):
-    plt.subplot(2, 2, i + 2)
+    plt.subplot(2, 3, i + 2)
     plt.imshow(hsv[:,:,i], cmap=plt.cm.binary)
     plt.title(HSV_TITLES[i])
+
+plt.subplot(2, 3, 5)
+plt.imshow(mask, cmap=plt.cm.binary)
+plt.title('Mask')
+
+mask_3c = np.zeros(image.shape, np.uint8)
+for i in range(3):
+    mask_3c[:,:,i] = mask
+plt.subplot(2, 3, 6)
+plt.imshow(cv2.cvtColor(cv2.bitwise_and(image, mask_3c), cv2.COLOR_BGR2RGB))
+plt.title('Image')
 
 plt.show()
