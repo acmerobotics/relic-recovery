e4c20f7e57a27672b0291904b42ee032c0c32d2e
Ryan Brott <rcbrott@gmail.com>
2017-10-06 21:48:26 -0700

    RR-21 use glyph information to add missing rails

--- scripts/cryptoboxanalyzer.py
+++ scripts/cryptoboxanalyzer.py
@@ -1,23 +1,21 @@
 import cv2
 import numpy as np
 import os
-from util import smart_hsv_range, resize_min_dim
+from util import smart_hsv_range
 from math import tan, pi
 
 BLUE, RED, BROWN, GRAY = tuple(range(4))
 RED_LOWER_HSV, RED_UPPER_HSV = (174, 80, 0), (4, 255, 255)
 BLUE_LOWER_HSV, BLUE_UPPER_HSV = (112, 80, 0), (124, 255, 255)
-# BROWN_LOWER_HSV, BROWN_UPPER_HSV = (1, 29, 55), (21, 77, 96)
-# GRAY_LOWER_HSV, GRAY_UPPER_HSV = (60, 2, 120), (120, 27, 198)
 BROWN_LOWER_HSV, BROWN_UPPER_HSV = (0, 31, 27), (19, 94, 104)
-# BROWN_LOWER_HSV, BROWN_UPPER_HSV = (0, 20, 60), (178, 96, 130)
 GRAY_LOWER_HSV, GRAY_UPPER_HSV = (66, 3, 121), (126, 69, 210)
 
 OPEN_KERNEL_SIZE, CLOSE_KERNEL_SIZE = 5, 5
 
 MIN_DIMENSION = 360
 
-ACTUAL_COL_WIDTH = 7.5  # in
+ACTUAL_RAIL_GAP = 7.5  # in
+ACTUAL_GLYPH_SIZE = 6.0 # in
 FOV_DEGREES = 58.5
 FOCAL_LENGTH_IN = 3.2 / 25.4  # in
 
@@ -54,44 +52,29 @@ for i, filename in enumerate(os.listdir(INPUT_DIR)):
     print("{}: \t{:.2f} R / {:.2f} B".format(filename, red_count / total, blue_count / total))
 
     if blue_count > red_count:
-        crypto_image = blue
+        rail_image = blue
         output_color = (255, 255, 0)
     else:
-        crypto_image = red
+        rail_image = red
         output_color = (0, 255, 255)
 
     # find cryptobox
-    crypto_morph = cv2.morphologyEx(crypto_image, cv2.MORPH_OPEN, OPEN_KERNEL)
-    crypto_morph = cv2.morphologyEx(crypto_morph, cv2.MORPH_CLOSE, CLOSE_KERNEL)
+    rail_morph = cv2.morphologyEx(rail_image, cv2.MORPH_OPEN, OPEN_KERNEL)
+    rail_morph = cv2.morphologyEx(rail_morph, cv2.MORPH_CLOSE, CLOSE_KERNEL)
 
-    crypto_contours = cv2.findContours(crypto_morph, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)[1]
+    rail_contours = cv2.findContours(rail_morph, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)[1]
 
-    cols = []
-    for contour in crypto_contours:
+    rail_xs = []
+    for contour in rail_contours:
         area = int(cv2.contourArea(contour))
         rect = cv2.boundingRect(contour)
         if (rect[2] / rect[3]) > 0.5 or area < 250:
             continue
         M = cv2.moments(contour)
         cx = int(M['m10'] / M['m00'])
-        cols.append(cx)
-        if output is not None:
-            # cv2.drawContours(output, [contour], 0, output_color, 2)
-            cv2.line(output, (4 * cx, 4 * rect[1]), (4 * cx, 4 * (rect[1] + rect[3])), output_color, 10)
-
-    cols.sort()
-    if len(cols) > 1:
-        col_width = (max(cols) - min(cols)) / (len(cols) - 1)
-        focal_length_px = (image_width * 0.5) / tan(FOV_DEGREES * 0.5 * pi / 180)
-        distance = (ACTUAL_COL_WIDTH * focal_length_px) / col_width
-        center = sum(cols) / len(cols)
-        dx = ((0.5 * image_width - center) * distance) / focal_length_px
-
-        cv2.putText(output, "{:.2f} in, {:.2f} in".format(distance, dx), (5, 50), cv2.FONT_HERSHEY_DUPLEX, 2, (0, 0, 255), 4)
-        # cv2.putText(output, str([int(i) for i in cols]), (5, 60), cv2.FONT_HERSHEY_DUPLEX, 1, (0, 0, 255), 2)
+        rail_xs.append(cx)
 
     # find glyphs
-
     full_glyphs, potential_glyphs, partial_glyphs = [], [], []
     glyph_width_sum, glyph_width_count = 0, 0
 
@@ -120,6 +103,7 @@ for i, filename in enumerate(os.listdir(INPUT_DIR)):
                 else:
                     potential_glyphs.append(rect)
 
+    mean_glyph_size = -1
     if glyph_width_count > 0:
         mean_glyph_size = glyph_width_sum / glyph_width_count
 
@@ -143,6 +127,63 @@ for i, filename in enumerate(os.listdir(INPUT_DIR)):
     for rect in full_glyphs:
         cv2.rectangle(output, (int(4 * rect[0]), int(4 * rect[1])),
                       (int(4 * (rect[0] + rect[2])), int(4 * (rect[1] + rect[3]))), (0, 255, 0), 10)
+        
+    # combine the rail and glyph info
+    rail_xs.sort()
+    print("{} {}".format(len(rail_xs), mean_glyph_size))
+    if 1 < len(rail_xs) < 4 or (len(rail_xs) == 1 and mean_glyph_size != -1):
+        if len(rail_xs) == 1:
+            print('using glyph size')
+            rail_gap = mean_glyph_size * ACTUAL_RAIL_GAP / ACTUAL_GLYPH_SIZE
+        else:
+            print('using calculated rail gap')
+            rail_gap = (max(rail_xs) - min(rail_xs)) / (len(rail_xs) - 1)
+        # fill left
+        while True:
+            x = rail_xs[0] - 0.5 * rail_gap
+            found_glyph = False
+            for glyph in full_glyphs:
+                if glyph[0] <= x <= (glyph[0] + glyph[2]):
+                    found_glyph = True
+                    break
+            if found_glyph:
+                print('found glyph, extending range')
+                rail_xs.insert(0, rail_xs[0] - rail_gap)
+            else:
+                print('didn\'t find glyph')
+                break
+        # fill right
+        while True:
+            x = rail_xs[-1] + 0.5 * rail_gap
+            found_glyph = False
+            for glyph in full_glyphs:
+                if glyph[0] <= x <= (glyph[0] + glyph[2]):
+                    found_glyph = True
+                    break
+            if found_glyph:
+                print('found glyph, extending range')
+                rail_xs.append(rail_xs[-1] + rail_gap)
+            else:
+                print('didn\'t find glyph')
+                break
+
+    for rail_x in rail_xs:
+        cv2.line(output, (int(4 * rail_x), 0), (int(4 * rail_x), 4 * image_height), output_color, 10)
+
+    # distance/offset calculation
+    if len(rail_xs) > 1:
+        rail_gap = (max(rail_xs) - min(rail_xs)) / (len(rail_xs) - 1)
+        focal_length_px = (image_width * 0.5) / tan(FOV_DEGREES * 0.5 * pi / 180)
+        distance = (ACTUAL_RAIL_GAP * focal_length_px) / rail_gap
+        if len(rail_xs) == 4:
+            center = sum(rail_xs) / len(rail_xs)
+            dx = ((0.5 * image_width - center) * distance) / focal_length_px
+
+            cv2.putText(output, "{:.2f} in, {:.2f} in".format(distance, dx), (5, 50), cv2.FONT_HERSHEY_DUPLEX, 2,
+                    (0, 0, 255), 4)
+        else:
+            cv2.putText(output, "{:.2f} in".format(distance), (5, 50), cv2.FONT_HERSHEY_DUPLEX, 2,
+                        (0, 0, 255), 4)
 
     # cv2.imwrite('{}/{}_brown.jpg'.format(OUTPUT_DIR, base_name), brown)
     # cv2.imwrite('{}/{}_gray.jpg'.format(OUTPUT_DIR, base_name), gray)
