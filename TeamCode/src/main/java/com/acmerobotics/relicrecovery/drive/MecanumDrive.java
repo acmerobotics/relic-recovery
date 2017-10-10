c0edf4e9e00495ae3fc0236ed22da580529a39f7
Kelly Muir <kellyrmuir@gmail.com>
2017-10-10 10:48:00 -0700

    RR-26 Improved readibility

--- TeamCode/src/main/java/com/acmerobotics/relicrecovery/drive/MecanumDrive.java
+++ TeamCode/src/main/java/com/acmerobotics/relicrecovery/drive/MecanumDrive.java
@@ -7,7 +7,7 @@ import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 
 /**
- * Created by kelly on 9/28/2017.
+ * Created by kelly on 9/28/2017
  *
  * Wheel layout (top view):
  *
@@ -18,23 +18,35 @@ import com.qualcomm.robotcore.hardware.HardwareMap;
  *      |       |
  *      |       |
  * (2)//---------\\(3)
+ *
+ * the paper: http://www.chiefdelphi.com/media/papers/download/2722
  */
 public class MecanumDrive {
 
     /***
-     * K = (wheelbase width + wheelbase height) / 4
+     * K = (wheelbase width + wheelbase height) / 4 (in)
      */
     public static double K = (18 + 18) / 4;
 
+    /**
+     * wheel radius (in)
+     */
+    public static double RADIUS = 2;
+
     private DcMotor[] motors;
     private int[] offsets;
+
+    public enum Side {
+        LEFT,
+        RIGHT;
+    }
 
     /**
      * construct drive with default configuration names
      * @param map hardware map
      */
     public MecanumDrive(HardwareMap map) {
-        this(map, new String[]{"frontLeft", "rearLeft", "rearRight", "frontRight"});
+        this(map, new String[]{"frontLeft", "rearLeft", "rearRight", "frontRight"}, Side.LEFT);
     }
 
     /**
@@ -42,27 +54,35 @@ public class MecanumDrive {
      * @param map hardware map
      * @param names names of the motors in the hardware mapping
      */
-    public MecanumDrive(HardwareMap map, String[] names) {
+    public MecanumDrive(HardwareMap map, String[] names, Side reversed) {
         if (names.length != 4) {
             throw new IllegalArgumentException("must be four for motors");
         }
         for (int i = 0; i < 4; i ++) {
-            motors[i] = map.get(DcMotor.class, names[i]);
+            motors[i] = map.dcMotor.get(names[i]);
         }
-        motors[0].setDirection(DcMotorSimple.Direction.REVERSE);
-        motors[1].setDirection(DcMotorSimple.Direction.REVERSE);
+
+        if (reversed == Side.LEFT) {
+            motors[0].setDirection(DcMotorSimple.Direction.REVERSE);
+            motors[1].setDirection(DcMotorSimple.Direction.REVERSE);
+        } else {
+            motors[2].setDirection(DcMotorSimple.Direction.REVERSE);
+            motors[3].setDirection(DcMotorSimple.Direction.REVERSE);
+        }
+
         resetEncoders();
     }
 
     /**
      * not as pretty but the exactly same result as the other version
+     * [W] is wheel rotations
      * [V] = [vX, vY, vOmega]'
      * [R] = [1, -1, -K]
      *       [1,  1, -K]
      *       [1, -1,  K]
      *       [1,  1,  K]
      *
-     * [F] = [V][R]
+     * [W] = [V][R]
      *
      * @param vel
      * @param omega
@@ -80,16 +100,25 @@ public class MecanumDrive {
 
     /**
      * get distance traveled from encoder values
-     * @param rot
-     * @return
+     *
+     * [W] is wheel rotations
+     * [V] = [vX, vY, vOmega]'
+     * [F] = [ 1/4,    1/4,   1/4,   1/4]
+     *       [-1/4,    1/4,  -1/4,   1/4]
+     *       [-1/4K, -1/4K,  1/4K,  1/4K]
+     *
+     * [V] = (r)[W][F]
+     *
+     * @param rot rotation of each wheel, in radians
+     * @return movement of robot
      */
     public static Pose2d getDelta (int[] rot) {
         if (rot.length != 4) {
             throw new IllegalArgumentException("length must be four");
         }
-        double x = ( rot[0] + rot[1] + rot[2] + rot[3]) / 4;
-        double y = (-rot[0] + rot[1] - rot[2] + rot[3]) / 4;
-        double h = (-rot[0] - rot[1] + rot[2] + rot[3]) / (4 * MecanumDrive.K);
+        double x = RADIUS * ( rot[0] + rot[1] + rot[2] + rot[3]) / 4;
+        double y = RADIUS * (-rot[0] + rot[1] - rot[2] + rot[3]) / 4;
+        double h = RADIUS * (-rot[0] - rot[1] + rot[2] + rot[3]) / (4 * MecanumDrive.K);
         return new Pose2d(x, y, h);
     }
 
---
+++ TeamCode/src/main/java/com/acmerobotics/relicrecovery/localization/LocalizationTest.java
@@ -0,0 +1,14 @@
+package com.acmerobotics.relicrecovery.localization;
+
+/**
+ * Created by kelly on 10/10/2017.
+ *
+ */
+
+public class LocalizationTest {
+
+    public static void main (String[] args) {
+        System.out.println("test");
+    }
+
+}
