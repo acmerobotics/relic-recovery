efd54346453db3f7cc72857a9815c3fd826b58df
Ryan Brott <rcbrott@gmail.com>
2017-10-27 22:55:23 -0700

    RR-31 add cryptobox unit test support

--- TeamCode/src/test/java/com/acmerobotics/relicrecovery/vision/CryptoboxTrackerTest.java
+++ TeamCode/src/test/java/com/acmerobotics/relicrecovery/vision/CryptoboxTrackerTest.java
@@ -1,7 +1,10 @@
 package com.acmerobotics.relicrecovery.vision;
 
 import org.junit.Test;
+import org.opencv.core.Mat;
+import org.opencv.imgcodecs.Imgcodecs;
 
+import java.io.File;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
@@ -23,6 +26,30 @@ public class CryptoboxTrackerTest {
         assertEquals(expectedOutputValues.size(), actualOutputValues.size());
         for (int i = 0; i < expectedOutputValues.size(); i++) {
             assertEquals(expectedOutputValues.get(i), actualOutputValues.get(i), 0.0001);
+        }
+    }
+
+    static {
+        System.load("/usr/local/Cellar/opencv/3.3.1/share/OpenCV/java/libopencv_java331.dylib");
+    }
+
+    @Test
+    public void testSampleImages() {
+        CryptoboxTracker tracker = new CryptoboxTracker(true);
+
+        File imageSourceDir = new File("scripts/cryptobox/images");
+        File imageOutputDir = new File("scripts/cryptobox/output");
+        imageOutputDir.mkdirs();
+
+        for (File imageFile : imageSourceDir.listFiles()) {
+            if (imageFile.getName().startsWith(".")) {
+                continue;
+            }
+            Mat image = Imgcodecs.imread(imageFile.getAbsolutePath());
+
+            tracker.analyzeCryptobox(image);
+
+            Imgcodecs.imwrite(new File(imageOutputDir, imageFile.getName()).getAbsolutePath(), image);
         }
     }
 
