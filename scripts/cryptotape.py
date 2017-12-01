f9b1944203afd090c2114dac5620f94aadc839b4
rbrott <rcbrott@gmail.com>
2017-11-30 20:59:17 -0800

    Delaunay triangulation

--- scripts/cryptotape.py
+++ scripts/cryptotape.py
@@ -85,6 +85,19 @@ def process_image(image, lower_hsv=BLUE_LOWER_HSV, upper_hsv=BLUE_UPPER_HSV):
     return points, outputs
 
 
+# Check if a point is inside a rectangle
+def rect_contains(rect, point):
+    if point[0] < rect[0]:
+        return False
+    elif point[1] < rect[1]:
+        return False
+    elif point[0] > rect[2]:
+        return False
+    elif point[1] > rect[3]:
+        return False
+    return True
+
+
 def main():
     for filename in listdir(INPUT_DIR_NAME):
         image = cv2.imread(INPUT_DIR_NAME + filename)
@@ -95,11 +108,39 @@ def main():
         # cv2.imwrite(OUTPUT_DIR_NAME + filename, output)
         for key, value in outputs.items():
             if 'output' in key:
-                point_scores = find_grid(points)
-                max_score = max(point_scores.values())
-                multiplier = 15 / max_score
-                for point, score in point_scores.items():
-                    cv2.circle(value, point, int(score * multiplier), (255, 255, 0), cv2.FILLED)
+                # point_scores = find_grid(points)
+                # max_score = max(point_scores.values())
+                # multiplier = 15 / max_score
+                # for point, score in point_scores.items():
+                #     cv2.circle(value, point, int(score * multiplier), (255, 255, 0), cv2.FILLED)
+                size = value.shape
+                r = (0, 0, size[1], size[0])
+                subdiv = cv2.Subdiv2D(r)
+                for point in points:
+                    subdiv.insert(point)
+                triangleList = subdiv.getTriangleList()
+
+                for t in triangleList:
+                    pt1 = (t[0], t[1])
+                    pt2 = (t[2], t[3])
+                    pt3 = (t[4], t[5])
+
+                    if rect_contains(r, pt1) and rect_contains(r, pt2) and rect_contains(r, pt3):
+                        slope1 = (abs(pt1[1] - pt2[1]) + 0.0001) / (abs(pt1[0] - pt2[0]) + 0.0001)
+                        slope2 = (abs(pt2[1] - pt3[1]) + 0.0001) / (abs(pt2[0] - pt3[0]) + 0.0001)
+                        slope3 = (abs(pt3[1] - pt1[1]) + 0.0001) / (abs(pt3[0] - pt1[0]) + 0.0001)
+                        if abs(log(slope1)) > log(3):
+                            cv2.line(value, pt1, pt2, (255, 0, 255), 5)
+                        if abs(log(slope2)) > log(3):
+                            cv2.line(value, pt2, pt3, (255, 0, 255), 5)
+                        if abs(log(slope3)) > log(3):
+                            cv2.line(value, pt3, pt1, (255, 0, 255), 5)
+
+                for point in points:
+                    cv2.circle(value, point, 10, (255, 255, 0), cv2.FILLED)
+            else:
+                continue
+
             output_filename = '{}{}_{}.jpg'.format(OUTPUT_DIR_NAME, filename.split('.')[0], key)
             if type(value) is list:
                 plt.figure()
