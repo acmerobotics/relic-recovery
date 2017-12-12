59472e6bd2456b276507802fe2415f081090ec0c
rbrott <rcbrott@gmail.com>
2017-12-11 21:47:21 -0800

    RR-46 add lateral tuner

---
+++ TeamCode/src/main/java/com/acmerobotics/relicrecovery/opmodes/tuner/LateralFFTuner.java
@@ -0,0 +1,94 @@
+package com.acmerobotics.relicrecovery.opmodes.tuner;
+
+import com.acmerobotics.library.dashboard.RobotDashboard;
+import com.acmerobotics.library.dashboard.telemetry.MultipleTelemetry;
+import com.acmerobotics.library.localization.Pose2d;
+import com.acmerobotics.relicrecovery.drive.DriveConstants;
+import com.acmerobotics.relicrecovery.drive.MecanumDrive;
+import com.acmerobotics.relicrecovery.loops.Looper;
+import com.acmerobotics.relicrecovery.path.LineSegment;
+import com.acmerobotics.relicrecovery.path.Path;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+import java.util.Arrays;
+
+/**
+ * @author Ryan
+ */
+
+@TeleOp(name = "Lateral FF Tuner")
+public class LateralFFTuner extends LinearOpMode {
+    public static final double LOWER_BOUND = 0;
+    public static final double UPPER_BOUND = 0.1;
+    public static final double DISTANCE = 48;
+
+    private RobotDashboard dashboard;
+    private Looper looper;
+    private MecanumDrive drive;
+    private volatile double value;
+
+    @Override
+    public void runOpMode() {
+        dashboard = RobotDashboard.getInstance();
+        telemetry = new MultipleTelemetry(telemetry, dashboard.getTelemetry());
+        drive = new MecanumDrive(hardwareMap, dashboard.getTelemetry());
+
+        looper = new Looper(20);
+        drive.registerLoops(looper);
+        looper.addLoop(((timestamp, dt) -> {
+            telemetry.addData("value", value);
+            telemetry.update();
+        }));
+        looper.start();
+
+        waitForStart();
+
+        double lower = LOWER_BOUND, upper = UPPER_BOUND;
+
+        while (opModeIsActive()) {
+            value = (lower + upper) / 2;
+
+            int numErrors = 3;
+            double errorSum = 0;
+            for (int i = 0; i < numErrors; i++) {
+                errorSum += testFeedforwardCoefficient(value);
+            }
+            double error = errorSum / numErrors;
+            if (error > 0) {
+                upper = value;
+            } else {
+                lower = value;
+            }
+        }
+    }
+
+    private double testFeedforwardCoefficient(double coefficient) {
+        DriveConstants.LATERAL_COEFFS.a = coefficient;
+
+        // reset heading + pose
+        drive.setEstimatedPose(new Pose2d(0, 0, drive.getHeading()));
+
+        Path forward = new Path(Arrays.asList(
+                new LineSegment(new Pose2d(0, 0, 0), new Pose2d(0, DISTANCE, 0))
+        ));
+        Path reverse = new Path(Arrays.asList(
+                new LineSegment(new Pose2d(0, DISTANCE, 0), new Pose2d(0, 0, 0))
+        ));
+        drive.followPath(forward);
+
+        while (opModeIsActive() && drive.isFollowingPath()) {
+            sleep(10);
+        }
+
+        double axialError = drive.getEstimatedPose().y() - DISTANCE;
+
+        drive.followPath(reverse);
+
+        while (opModeIsActive() && drive.isFollowingPath()) {
+            sleep(10);
+        }
+
+        return axialError;
+    }
+}
