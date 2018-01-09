e428d20c9734bcd476873cfaa4c6ccfeef2a4489
Ryan Brott <rcbrott@gmail.com>
2018-01-08 22:38:02 -0800

    Add selection activity for changing detectors

--- VisionTest/src/main/java/com/acmerobotics/relicrecovery/vision/MainActivity.java
+++ VisionTest/src/main/java/com/acmerobotics/relicrecovery/vision/MainActivity.java
@@ -1,8 +1,12 @@
 package com.acmerobotics.relicrecovery.vision;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Spinner;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,10 +14,20 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
+        final Spinner spinner = findViewById(R.id.spinner);
 
+        Button button = findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, CameraActivity.class);
+                intent.putExtra("detector", spinner.getSelectedItem().toString());
+                startActivity(intent);
+            }
+        });
     }
 
 }
