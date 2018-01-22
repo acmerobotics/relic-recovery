bfb9e69cc237ab90d10a465c4cc30cf55a04d222
rbrott <rcbrott@gmail.com>
2018-01-21 23:15:09 -0800

    RR-54 add new dump bed driver

--- TeamCode/src/main/java/com/acmerobotics/relicrecovery/subsystems/DumpBed.java
+++ TeamCode/src/main/java/com/acmerobotics/relicrecovery/subsystems/DumpBed.java
@@ -1,6 +1,14 @@
 package com.acmerobotics.relicrecovery.subsystems;
 
 import com.acmerobotics.library.dashboard.config.Config;
+import com.acmerobotics.library.util.TimestampedData;
+import com.acmerobotics.relicrecovery.motion.MotionConstraints;
+import com.acmerobotics.relicrecovery.motion.MotionGoal;
+import com.acmerobotics.relicrecovery.motion.MotionProfile;
+import com.acmerobotics.relicrecovery.motion.MotionProfileGenerator;
+import com.acmerobotics.relicrecovery.motion.MotionState;
+import com.acmerobotics.relicrecovery.motion.PIDFCoefficients;
+import com.acmerobotics.relicrecovery.motion.PIDFController;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DigitalChannel;
 import com.qualcomm.robotcore.hardware.HardwareMap;
@@ -8,89 +16,148 @@ import com.qualcomm.robotcore.hardware.Servo;
 
 import org.firstinspires.ftc.robotcore.external.Telemetry;
 
+import java.util.LinkedHashMap;
+import java.util.Map;
+
 @Config
 public class DumpBed extends Subsystem {
-    public static final double LIFT_POWER_DOWN = -0.3;
-    public static final double LIFT_POWER_UP = 0.6;
+    public static final double LIFT_HEIGHT = 0; // TODO: determine
+    public static final double PULLEY_RADIUS = 0; // TODO: determine
 
-    public static final double LEFT_ROTATE_DOWN_POS = 0.39;
-    public static final double LEFT_ROTATE_UP_POS = 0.86;
+    public static MotionConstraints LIFT_CONSTRAINTS = new MotionConstraints(0, 0, 0, MotionConstraints.EndBehavior.OVERSHOOT);
+    public static PIDFCoefficients LIFT_PIDF = new PIDFCoefficients(0, 0, 0, 0, 0);
 
-    public static final double RELEASE_ENGAGE_POS = 0.61;
-    public static final double RELEASE_DISENGAGE_POS = 0;
+    public static double LIFT_CALIBRATION_POWER = -0.2;
+    public static double LIFT_PROFILE_MIN_POWER = 0.2;
+
+    public static double ROTATE_HEIGHT = 0;
+
+    public static double LEFT_ROTATE_DOWN_POS = 0.39;
+    public static double LEFT_ROTATE_UP_POS = 0.86;
+
+    public static double RELEASE_ENGAGE_POS = 0.61;
+    public static double RELEASE_DISENGAGE_POS = 0;
 
     public enum Mode {
-        NORMAL,
-        MOVE_DOWN,
-        MOVE_UP
+        STATIC,
+        CALIBRATE,
+        MOTION_PROFILE
     }
 
-    private Mode mode = Mode.NORMAL;
+    private Mode mode = Mode.STATIC;
 
-    private DcMotor dumpLiftLeft, dumpLiftRight;
+    private DcMotor liftMotor;
     private Servo dumpRelease, dumpRotateLeft, dumpRotateRight;
-    private DigitalChannel dumpLiftBottomTouch, dumpLiftTopTouch;
+    private DigitalChannel liftMagneticTouch;
 
-    private boolean liftDumping, liftDown;
+    private MotionProfile profile;
+    private PIDFController profileController;
+
+    private boolean liftDumping, releaseEngaged, calibrated, snapToEndpoint, movingDown;
+    private double encoderOffset, dumpRotation;
+
+    public static final String[] CONDITIONAL_TELEMETRY_KEYS = {
+            "dumpTouchPressed",
+            "dumpLiftHeight",
+            "dumpLiftHeightError",
+            "dumpLiftHeightUpdate",
+            "dumpLiftPower"
+    };
 
     private Telemetry telemetry;
+    private LinkedHashMap<String, Object> telemetryMap;
 
     public DumpBed(HardwareMap map, Telemetry telemetry) {
         this.telemetry = telemetry;
+        telemetryMap = new LinkedHashMap<>();
+        for (String key : CONDITIONAL_TELEMETRY_KEYS) {
+            telemetryMap.put(key, 0);
+        }
 
-        dumpLiftLeft = map.dcMotor.get("dumpLiftLeft");
-        dumpLiftRight = map.dcMotor.get("dumpLiftRight");
+        profileController = new PIDFController(LIFT_PIDF);
 
-        dumpRotateLeft = map.servo.get("dumpRotateLeft");
-        dumpRotateRight = map.servo.get("dumpRotateRight");
-        dumpRelease = map.servo.get("dumpRelease");
+        liftMotor = map.dcMotor.get("liftMotor");
 
-        dumpLiftBottomTouch = map.digitalChannel.get("dumpLiftBottomTouch");
-        dumpLiftBottomTouch.setMode(DigitalChannel.Mode.INPUT);
-        dumpLiftTopTouch = map.digitalChannel.get("dumpLiftTopTouch");
-        dumpLiftTopTouch.setMode(DigitalChannel.Mode.INPUT);
+        dumpRotateLeft = map.servo.get("bedRotateLeft");
+        dumpRotateRight = map.servo.get("bedRotateRight");
+        dumpRelease = map.servo.get("bedRelease");
+
+        liftMagneticTouch = map.digitalChannel.get("liftMagneticTouch");
+        liftMagneticTouch.setMode(DigitalChannel.Mode.INPUT);
 
         engageRelease();
-    }
-
-    private void setLiftPower(double power) {
-        dumpLiftLeft.setPower(power);
-        dumpLiftRight.setPower(power);
-    }
-
-    public void liftDown() {
-        liftDown = true;
-        mode = Mode.MOVE_DOWN;
-        setLiftPower(LIFT_POWER_DOWN);
-    }
-
-    public void liftUp() {
-        liftDown = false;
-        mode = Mode.MOVE_UP;
-        setDumpRotation(0.5);
-        setLiftPower(LIFT_POWER_UP);
     }
 
     public Mode getMode() {
         return mode;
     }
 
-    public boolean isLiftDown() {
-        return liftDown;
+    public double getEncoderPosition() {
+        return liftMotor.getCurrentPosition() - encoderOffset;
+    }
+
+    public void setEncoderPosition(int position) {
+        encoderOffset = liftMotor.getCurrentPosition() - position;
+    }
+
+    public void resetEncoder() {
+        setEncoderPosition(0);
+    }
+
+    public boolean isCalibrated() {
+        return calibrated;
+    }
+
+    public void calibrate() {
+        if (!calibrated) {
+            mode = Mode.CALIBRATE;
+        }
+    }
+
+    private void setLiftPower(double power) {
+        liftMotor.setPower(power);
+    }
+
+    public double getLiftHeight() {
+        double position = getEncoderPosition();
+        return 2 * Math.PI * PULLEY_RADIUS * position;
+    }
+
+    public void setLiftHeight(double height) {
+        setEncoderPosition((int) (height / (2 * Math.PI * PULLEY_RADIUS)));
+    }
+
+    public void moveLiftToHeight(double height) {
+        if (!calibrated) return;
+        double startingHeight = getLiftHeight();
+        MotionState start = new MotionState(startingHeight, 0, 0, 0, TimestampedData.getCurrentTime());
+        MotionGoal goal = new MotionGoal(height, 0);
+        profile = MotionProfileGenerator.generateProfile(start, goal, LIFT_CONSTRAINTS);
+        mode = Mode.MOTION_PROFILE;
+        snapToEndpoint = (height == 0 || height == LIFT_HEIGHT);
+        movingDown = height < startingHeight;
+        profileController.reset();
     }
 
     private void setDumpRotation(double rot) {
         double servoPosition = LEFT_ROTATE_DOWN_POS + rot * (LEFT_ROTATE_UP_POS - LEFT_ROTATE_DOWN_POS);
         dumpRotateLeft.setPosition(servoPosition);
         dumpRotateRight.setPosition(1 - servoPosition);
+        this.dumpRotation = rot;
     }
 
     private void engageRelease() {
-        dumpRelease.setPosition(RELEASE_ENGAGE_POS);
+        if (!releaseEngaged) {
+            dumpRelease.setPosition(RELEASE_ENGAGE_POS);
+            releaseEngaged = true;
+        }
     }
 
     private void disengageRelease() {
-        dumpRelease.setPosition(RELEASE_DISENGAGE_POS);
+        if (releaseEngaged) {
+            dumpRelease.setPosition(RELEASE_DISENGAGE_POS);
+            releaseEngaged = false;
+        }
     }
 
     public boolean isDumping() {
@@ -108,40 +175,91 @@ public class DumpBed extends Subsystem {
     public void retract() {
         if (liftDumping) {
             liftDumping = false;
-            if (liftDown) {
-                setDumpRotation(0);
-            } else {
+            if (getLiftHeight() > ROTATE_HEIGHT) {
                 setDumpRotation(0.5);
+            } else {
+                setDumpRotation(0);
             }
             engageRelease();
         }
     }
 
     public void update() {
-        telemetry.addData("dumpMode", mode);
+        telemetryMap.put("dumpMode", mode);
+        telemetryMap.put("dumpCalibrated", calibrated);
+        telemetryMap.put("dumpLiftDumping", liftDumping);
+        telemetryMap.put("dumpReleaseEngaged", releaseEngaged);
+        telemetryMap.put("dumpLiftSnap", snapToEndpoint);
+        telemetryMap.put("dumpRotation", dumpRotation);
+
         switch (mode) {
-            case NORMAL:
+            case STATIC:
                 // do nothing
                 break;
-            case MOVE_DOWN:
-                boolean bottomTouch = !dumpLiftBottomTouch.getState();
-                telemetry.addData("dumpLiftButtonTouch", bottomTouch);
-                if (bottomTouch) {
-                    mode = Mode.NORMAL;
+            case CALIBRATE: {
+                boolean magneticTouchPressed = !liftMagneticTouch.getState();
+                telemetryMap.put("dumpTouchPressed", magneticTouchPressed);
+                if (magneticTouchPressed) {
                     setLiftPower(0);
-                    if (!liftDumping) {
-                        setDumpRotation(0);
+                    resetEncoder();
+                    calibrated = true;
+                    mode = Mode.STATIC;
+                } else {
+                    setLiftPower(LIFT_CALIBRATION_POWER);
+                }
+                break;
+            }
+            case MOTION_PROFILE: {
+                boolean magneticTouchPressed = !liftMagneticTouch.getState();
+                telemetryMap.put("dumpTouchPressed", magneticTouchPressed);
+                if (magneticTouchPressed) {
+                    setLiftPower(0);
+                    mode = Mode.STATIC;
+                    if (movingDown) {
+                        setLiftHeight(0);
+                    } else {
+                        setLiftHeight(LIFT_HEIGHT);
                     }
                 }
-                break;
-            case MOVE_UP:
-                boolean topTouch = !dumpLiftTopTouch.getState();
-                telemetry.addData("dumpLiftTopTouch", topTouch);
-                if (topTouch) {
-                    mode = Mode.NORMAL;
-                    setLiftPower(0);
+                double timestamp = TimestampedData.getCurrentTime();
+                if (timestamp > profile.end().t) {
+                    if (snapToEndpoint) {
+                        setLiftPower((movingDown ? -1 : 1) * LIFT_PROFILE_MIN_POWER);
+                    } else {
+                        setLiftPower(0);
+                        mode = Mode.STATIC;
+                    }
+                } else {
+                    MotionState motionState = profile.get(timestamp);
+                    profileController.setSetpoint(motionState);
+                    double liftHeight = getLiftHeight();
+                    telemetryMap.put("dumpLiftHeight", liftHeight);
+                    if (!liftDumping) {
+                        if (dumpRotation > 0.25 && liftHeight < ROTATE_HEIGHT) {
+                            setDumpRotation(0);
+                        } else if (dumpRotation < 0.25 && liftHeight > ROTATE_HEIGHT) {
+                            setDumpRotation(0.5);
+                        }
+                    }
+                    double heightError = liftHeight - motionState.x;
+                    telemetryMap.put("dumpLiftHeightError", heightError);
+                    double liftUpdate = profileController.update(heightError);
+                    telemetryMap.put("dumpLiftHeightUpdate", liftUpdate);
+                    double liftPower;
+                    if (snapToEndpoint) {
+                        liftPower = Math.max(liftUpdate, Math.signum(liftUpdate) * LIFT_PROFILE_MIN_POWER);
+                    } else {
+                        liftPower = liftUpdate;
+                    }
+                    telemetryMap.put("dumpLiftPower", liftPower);
+                    setLiftPower(liftPower);
                 }
                 break;
+            }
+        }
+
+        for (Map.Entry<String, Object> entry : telemetryMap.entrySet()) {
+            telemetry.addData(entry.getKey(), entry.getValue());
         }
     }
 }
