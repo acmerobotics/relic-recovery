a3e8f29e9de5229ba5c64872fe18100db3df77f3
Ryan Brott <rcbrott@gmail.com>
2018-02-02 07:42:25 -0800

    Fix smoothing ratios and add some loop delay

--- TeamCode/src/main/java/com/acmerobotics/relicrecovery/opmodes/test/UltrasonicTest.java
+++ TeamCode/src/main/java/com/acmerobotics/relicrecovery/opmodes/test/UltrasonicTest.java
@@ -11,7 +11,7 @@ import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
 
 @TeleOp
 public class UltrasonicTest extends OpMode {
-    public static final double[] SMOOTHING_RATIOS = {1, 0.5, 0.95, 0.99, 0.999};
+    public static final double[] SMOOTHING_RATIOS = {1, 0.05, 0.01, 0.001, 0.0001};
 
     private MaxSonarEZ1UltrasonicSensor ultrasonicSensor;
     private ExponentialSmoother[] smoothers;
@@ -31,6 +31,11 @@ public class UltrasonicTest extends OpMode {
         double distance = ultrasonicSensor.getDistance(DistanceUnit.INCH);
         for (int i = 0; i < smoothers.length; i++) {
             telemetry.addData("distance" + i, smoothers[i].update(distance));
+        }
+        try {
+            Thread.sleep(50);
+        } catch (InterruptedException e) {
+            Thread.currentThread().interrupt();
         }
     }
 }
