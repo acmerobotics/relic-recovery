da59b752729e64231213f9d780ca3a3ea90374f9
Ryan Brott <rcbrott@gmail.com>
2018-02-06 15:51:32 -0800

    Adapt relic driver to allow for dashboard-configurable positions

--- TeamCode/src/main/java/com/acmerobotics/relicrecovery/subsystems/RelicRecoverer.java
+++ TeamCode/src/main/java/com/acmerobotics/relicrecovery/subsystems/RelicRecoverer.java
@@ -1,5 +1,6 @@
 package com.acmerobotics.relicrecovery.subsystems;
 
+import com.acmerobotics.library.dashboard.config.Config;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 import com.qualcomm.robotcore.hardware.Servo;
@@ -10,27 +11,28 @@ import org.firstinspires.ftc.robotcore.external.Telemetry;
  * Created by ryanbrott on 1/11/18.
  */
 
+@Config
 public class RelicRecoverer extends Subsystem {
+    public static double WRIST_STOW_POSITION = 0.04;
+    public static double WRIST_UP_POSITION = 1;
+    public static double WRIST_DOWN_POSITION = 0.5;
+
+    public static double FINGER_CLOSE_POSITION = 0.61;
+    public static double FINGER_OPEN_POSITION = 0.35;
+
     private Telemetry telemetry;
 
     public enum WristPosition {
-        STOW(0.04),
-        UP(1),
-        DOWN(0.5);
-
-        private double position;
-
-        WristPosition(double position) {
-            this.position = position;
-        }
-
-        public double getServoPosition() {
-            return position;
-        }
+        STOW,
+        UP,
+        DOWN
     }
 
     private DcMotor relicExtend;
     private Servo relicWrist, relicFinger;
+
+    private WristPosition wristPosition;
+    private boolean fingerClosed;
 
     public RelicRecoverer(HardwareMap map, Telemetry telemetry) {
         this.telemetry = telemetry;
@@ -49,19 +51,38 @@ public class RelicRecoverer extends Subsystem {
     }
 
     public void setWristPosition(WristPosition position) {
-        relicWrist.setPosition(position.getServoPosition());
+        wristPosition = position;
     }
 
     public void closeFinger() {
-        relicFinger.setPosition(0.61);
+        fingerClosed = true;
     }
 
     public void openFinger() {
-        relicFinger.setPosition(0.35);
+        fingerClosed = false;
     }
 
     @Override
     public void update() {
+        telemetry.addData("relicWristPosition", wristPosition);
+        telemetry.addData("relicFingerClosed", fingerClosed);
 
+        switch (wristPosition) {
+            case STOW:
+                relicWrist.setPosition(WRIST_STOW_POSITION);
+                break;
+            case UP:
+                relicWrist.setPosition(WRIST_UP_POSITION);
+                break;
+            case DOWN:
+                relicWrist.setPosition(WRIST_DOWN_POSITION);
+                break;
+        }
+
+        if (fingerClosed) {
+            relicFinger.setPosition(FINGER_CLOSE_POSITION);
+        } else {
+            relicFinger.setPosition(FINGER_OPEN_POSITION);
+        }
     }
 }
