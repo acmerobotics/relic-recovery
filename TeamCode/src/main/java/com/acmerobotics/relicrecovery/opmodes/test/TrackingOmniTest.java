350392302e6af9119e43f32623e9e9e3a9e8b05d
Ryan Brott <rbrott@users.noreply.github.com>
2018-04-14 10:42:00 -0700

    RR-72 tracking omnis (#40)
    * RR-72 add tracking omni localizer logic
    
    * RR-72 add tracking encoder hardware interface
    
    * RR-72 fix tracking omni localization (theoretically)
    
    * RR-72 add tracking omni test
    
    * RR-72 fix localization logic (hopefully)
    
    * RR-72 initial tracking omni test

---
+++ TeamCode/src/main/java/com/acmerobotics/relicrecovery/opmodes/test/TrackingOmniTest.java
@@ -0,0 +1,38 @@
+package com.acmerobotics.relicrecovery.opmodes.test;
+
+import com.acmerobotics.library.localization.Vector2d;
+import com.acmerobotics.relicrecovery.localization.TrackingOmniLocalizer;
+import com.acmerobotics.relicrecovery.subsystems.Robot;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+@TeleOp
+public class TrackingOmniTest extends LinearOpMode {
+    @Override
+    public void runOpMode() {
+        Robot robot = new Robot(this);
+        robot.drive.setLocalizer(new TrackingOmniLocalizer(robot.drive));
+        robot.drive.enablePositionEstimation();
+        robot.start();
+
+        waitForStart();
+
+        while (opModeIsActive()) {
+            double x, y = 0, omega;
+
+            x = -gamepad1.left_stick_y;
+
+            if (Math.abs(gamepad1.left_stick_x) > 0.5) {
+                y = -gamepad1.left_stick_x;
+            }
+
+            omega = -gamepad1.right_stick_x;
+
+            if (gamepad1.left_trigger != 0 || gamepad1.right_trigger != 0) {
+                y = (gamepad1.left_trigger - gamepad1.right_trigger) / 4.0;
+            }
+
+            robot.drive.setVelocity(new Vector2d(x, y), omega);
+        }
+    }
+}
